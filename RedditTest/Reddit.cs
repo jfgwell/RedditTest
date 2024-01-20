666dc3f6cf7fccd0a68284e968a1bf94582c5b48
jfgwell <157252721+jfgwell@users.noreply.github.com>
2024-01-20 17:52:44 -0500

    Add files via upload

---
+++ RedditTest/Reddit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Reddit;
+using Reddit.Controllers.Internal;
+using RedditNet;
+
+namespace RedditTest
+{
+    public class Reddit
+    {
+        RedditNet.Auth.RedditPasswordAuth RA = new RedditNet.Auth.RedditPasswordAuth(null, null, null, null, null);
+        RedditApi reddit = new RedditApi(); 
+        
+        
+       
+    }
+}
