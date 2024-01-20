d64197410eba1c91be286316b80123c6f2cd693d
jfgwell <157252721+jfgwell@users.noreply.github.com>
2024-01-20 18:58:56 -0500

    Add files via upload

---
+++ SubredditLogic/ISubredditLogic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubredditLogic
+{
+    internal interface ISubredditLogic
+    {
+        public void Setup();
+
+        public void Process();
+    }
+}
---
+++ SubredditLogic/Interval.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubredditLogic
+{
+    public class Interval
+    {
+        private static System.Timers.Timer timer;
+        private static SubRedditLogic redditLogic;
+
+        public void Setup()
+        {
+            redditLogic = new SubRedditLogic();
+            redditLogic.Setup();
+        }
+        public void Run()
+        {
+            Timer timer = new Timer(TimerCallback, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+
+
+            // Keep the program running
+            Console.WriteLine("Press Enter to exit.");
+            Console.ReadLine();
+        }
+
+
+        private static void TimerCallback(object state)
+        {
+            // Call the Process() method at the specified interval
+            redditLogic.Process();
+        }
+    }
+}
